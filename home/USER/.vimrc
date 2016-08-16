cb2ed421abcd3dfe030dcef4aaabe2d4aa25574a
Jacques Uber <uberj@miradorfin.com>
2016-08-16 13:29:51 -0700

    hooks

--- home/USER/.vimrc
+++ home/USER/.vimrc
@@ -155,3 +155,11 @@ au BufNewFile,BufRead *.py
     \ set expandtab |
     \ set autoindent |
     \ set fileformat=unix
+
+au BufNewFile,BufRead *.sh
+    \ set tabstop=4 |
+    \ set softtabstop=4 |
+    \ set shiftwidth=4 |
+    \ set expandtab |
+    \ set autoindent |
+    \ set fileformat=unix
--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Using \$HOME: $HOME"
-for file in $(git ls-files | grep -E '^home/uberj')
+for file in $(git ls-files | grep -E '^home/USER')
 do
-        target=$(echo $file | sed "s@home/uberj@$HOME@")
+        target=$(echo $file | sed "s@home/USER@$HOME@")
         set -x
         cp $target $file
         set +x
